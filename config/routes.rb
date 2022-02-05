6759b84f71eae77a83a28d396febb7dddb86d6c5
Kamran Rizwan <kamranrizwan0925@gmail.com>
2022-02-06 01:16:35 +0500

    Complete Authentication System

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :articles
   root 'home#index' # shortcut for the above
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
